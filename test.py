d9eddb9b9c13a0d7f6fb4cbd696bd2afeb9ab4d2
xxf1995 <xiax@carleton.edu>
2016-11-29 03:37:05 -0600

    clean up

--- test.py
+++ test.py
@@ -1,8 +1,40 @@
-import urllib2
-request = urllib2.Request(
-    r'http://google.com',
-    headers={'User-Agent':'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 firefox/2.0.0.11'})
-page = urllib2.urlopen(request)
+import sys
+import time
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtWebKit import *
 
-with open('somefile.png','wb') as f:
-    f.write(page.read())
+class Screenshot(QWebView):
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        QWebView.__init__(self)
+        self._loaded = False
+        self.loadFinished.connect(self._loadFinished)
+
+    def capture(self, url, output_file):
+        self.load(QUrl(url))
+        self.wait_load()
+        # set to webpage size
+        frame = self.page().mainFrame()
+        self.page().setViewportSize(frame.contentsSize())
+        # render image
+        image = QImage(self.page().viewportSize(), QImage.Format_ARGB32)
+        painter = QPainter(image)
+        frame.render(painter)
+        painter.end()
+        print 'saving', output_file
+        image.save(output_file)
+
+    def wait_load(self, delay=0):
+        # process app events until page loaded
+        while not self._loaded:
+            self.app.processEvents()
+            time.sleep(delay)
+        self._loaded = False
+
+    def _loadFinished(self, result):
+        self._loaded = True
+
+s = Screenshot()
+s.capture('http://webscraping.com', 'website.png')
+s.capture('http://webscraping.com/blog', 'blog.png')
